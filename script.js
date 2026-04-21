3a59e89ac907da8ad6a55fd265c2cde65ad1c11e
Michael <michaelpakpahan41@gmail.com>
2026-04-21 12:57:15 +0700

    feat: add dark mode switch feature

--- script.js
+++ script.js
@@ -104,4 +104,20 @@ function displayEntries() {
     });
 }
 
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+}
+
 displayEntries();
